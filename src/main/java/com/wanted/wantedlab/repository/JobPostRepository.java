a43d58dd767404bb30b39e55d64c03f946ba5b06
johan1103 <johan1103@naver.com>
2023-10-08 01:03:58 +0900

    [Fix] N+1 problem #6

--- src/main/java/com/wanted/wantedlab/repository/JobPostRepository.java
+++ src/main/java/com/wanted/wantedlab/repository/JobPostRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface JobPostRepository extends JpaRepository<JobPost,Long> {
-  @Query("select j from JobPost j")
+  @Query("select j from JobPost j join fetch j.company")
   Slice<JobPost> getJobPostSlice(PageRequest pageRequest);
 }
