c413da70131d047e71436808f574e2e79da7708d
johan1103 <johan1103@naver.com>
2023-10-08 01:22:54 +0900

    [Feat] query for JobPosts

--- src/main/java/com/wanted/wantedlab/repository/JobPostRepository.java
+++ src/main/java/com/wanted/wantedlab/repository/JobPostRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface JobPostRepository extends JpaRepository<JobPost,Long> {
   @Query("select j from JobPost j join fetch j.company")
   Slice<JobPost> getJobPostSlice(PageRequest pageRequest);
+  @Query("select j from JobPost j join fetch j.company c where c.id=:company_id")
+  List<JobPost> getJobPostsByCompanyId(@Param("company_id")Long companyId);
 }
