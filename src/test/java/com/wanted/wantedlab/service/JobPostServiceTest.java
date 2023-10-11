558efdf31647f59a9fdd48c378a8e08b9572e4fd
johan1103 <johan1103@naver.com>
2023-10-12 00:20:17 +0900

    [Test] delete(empty letter) success

--- src/test/java/com/wanted/wantedlab/service/JobPostServiceTest.java
+++ src/test/java/com/wanted/wantedlab/service/JobPostServiceTest.java
@@ -1,7 +1,10 @@
 package com.wanted.wantedlab.service;
 
+import com.wanted.wantedlab.dto.jobPost.request.JobPostDeleteRequest;
 import com.wanted.wantedlab.dto.jobPost.request.JobPostUploadRequest;
+import com.wanted.wantedlab.dto.jobPost.response.JobPostDeleteResult;
 import com.wanted.wantedlab.dto.jobPost.response.JobPostUploadResult;
+import com.wanted.wantedlab.entity.ApplicationLetter;
 import com.wanted.wantedlab.entity.Company;
 import com.wanted.wantedlab.entity.JobPost;
 import com.wanted.wantedlab.repository.ApplicationLetterRepository;
@@ -15,6 +18,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -56,5 +62,35 @@ public class JobPostServiceTest {
     assertThat(result).usingRecursiveComparison().isEqualTo(expectedResult);
     verify(entityValidator, times(1)).validateCompany(anyLong());
     verify(jobPostRepository, times(1)).save(Mockito.any());
+  }
+
+  @Test
+  @DisplayName("delete 성공 테스트 [해당 공고 지원자가 없을 때]")
+  void delete_success_empty_letter(){
+    //given
+    JobPostDeleteRequest request = new JobPostDeleteRequest(1L);
+    Company sampleCompany = new Company(1L,"sample-company","sample-country","sample-region");
+    JobPost sampleJobPost = new JobPost(1L,"sample-position","sample-content",
+            "sample-skills",100000,sampleCompany);
+    List<ApplicationLetter> emptyLetters = new ArrayList<>();
+
+    when(entityValidator.validateJobPost(anyLong())).thenReturn(sampleJobPost);
+    when(applicationLetterRepository.findDeleteApplicationLetter(anyLong())).thenReturn(emptyLetters);
+    when(deletedApplicationLetterRepository.saveAll(any())).thenReturn(null);
+    doNothing().when(applicationLetterRepository).deleteLetterAssociatedJobPost(any());
+    doNothing().when(jobPostRepository).delete(any());
+
+    JobPostDeleteResult expectedResult = new JobPostDeleteResult(true);
+
+    //when
+    JobPostDeleteResult result = jobPostService.delete(request);
+
+    //then
+    assertThat(result).usingRecursiveComparison().isEqualTo(expectedResult);
+    verify(entityValidator,times(1)).validateJobPost(anyLong());
+    verify(applicationLetterRepository,times(1)).findDeleteApplicationLetter(anyLong());
+    verify(deletedApplicationLetterRepository,times(1)).saveAll(any());
+    verify(applicationLetterRepository,times(1)).deleteLetterAssociatedJobPost(any());
+    verify(jobPostRepository,times(1)).delete(any());
   }
 }
